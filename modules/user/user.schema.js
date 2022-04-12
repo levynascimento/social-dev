3873aa89be27843b6ea54cd0a78cb7ac459b5fe8
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-04-12 20:15:59 -0300

    Otimizado código do form de cadastro e input

--- modules/user/user.schema.js
+++ modules/user/user.schema.js
@@ -5,5 +5,5 @@ export const signupSchema = Joi.object({
   lastName: Joi.string().required().max(50).message('O campo "sobrenome" pode ter no máximo {{#limit}} caracteres.'),
   user: Joi.string().required().max(30).message('O campo "usuário" pode ter no máximo {{#limit}} caracteres.'),
   email: Joi.string().email({ tlds: { allow: false } }).required().max(100).message('O campo "email" pode ter no máximo {{#limit}} caracteres.'),
-  password: Joi.string().required().max(50).message('O campo "senha" pode ter no máximo {{#limit}} caracteres.').min(6).message('O campo "senha" precisa ter no mínimo{{#limit}} caracteres.'),
+  password: Joi.string().required().max(50).message('O campo "senha" pode ter no máximo {{#limit}} caracteres.').min(6).message('O campo "senha" precisa ter no mínimo {{#limit}} caracteres.'),
 })
